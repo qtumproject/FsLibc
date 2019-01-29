bf85dad80522c559e6c7bdeabc11d1168a32626a
Jordan Earls <earlz@earlz.net>
2019-01-28 17:12:02 -0700

    Add malloc defines

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -3,4 +3,23 @@
 
 #include "fslc/fslc_stdlib.h"
 
+#define malloc __qtum_malloc
+#define free __qtum_free
+#define realloc __qtum_realloc
+#define calloc __qtum_calloc
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+//implemented in libqtum
+void* __qtum_malloc(unsigned int sz);
+void* __qtum_calloc(unsigned int sz);
+void __qtum_free(void* mem);
+void* __qtum_realloc(void* mem, unsigned int newsz);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
 #endif /* _STD_STDLIB_H */
